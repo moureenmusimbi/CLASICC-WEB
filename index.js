d95d95d829a2dad151df554f2c94d259f6e22ead
moureenmusimbi <musimbimoureen4@gmail.com>
2023-11-03 22:58:53 +0300

    My new commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//let firstname = "Mary"
+/*let secondname = "wambui"
+console.log(firstname + "" + secondname)
+
+
+// calculator
+let fnumber = 100;
+let ssnumber = 10;
+let addition = fnumber + ssnumber;
+let multiply = fnumber * ssnumber;
+let divide = fnumber / ssnumber;
+
+console.log("addition", addition);
+console.log("multiply", multiply);
+console.log("divide", divide);*/
+console.log()
